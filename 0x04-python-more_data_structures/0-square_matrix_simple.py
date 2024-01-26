0e5789ec37f140ba329c8a507c365c4f54054ee7
Ustaz07 <shamsutukor@gmail.com>
2024-01-26 11:37:10 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [[x**2 for x in row] for row in matrix]
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is not None and isinstance(a_dictionary, dict):
+        for key in sorted(a_dictionary.keys()):
+            print("{}: {}".format(key, a_dictionary[key]))
 
 # Example usage
 if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    new_matrix = square_matrix_simple(matrix)
-    print(new_matrix)
-    print(matrix)
-
+    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+    print_sorted_dictionary(a_dictionary)
