4e22c7b95718889ed119f92c55b1ae4fb2b4fbd2
Ustaz07 <shamsutukor@gmail.com>
2024-01-26 11:59:48 -0800

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None and isinstance(a_dictionary, dict):
-        sorted_keys = sorted(a_dictionary.keys())
-        for key in sorted_keys:
-            print("{}: {}".format(key, a_dictionary[key]))
+    sorted_dict = sorted(a_dictionary)
+    for i in sorted_dict:
+        print("{}: {}".format(i, a_dictionary[i]))
 
 # Example usage
 if __name__ == "__main__":
