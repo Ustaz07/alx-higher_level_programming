d1c8d8e2bac4ffbd5897746f80e2ef16252f2e0b
Ustaz07 <shamsutukor@gmail.com>
2024-03-27 21:58:24 -0700

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
