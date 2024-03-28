7e49762cac5adab8545acce05611ad9d85a6ab20
Ustaz07 <shamsutukor@gmail.com>
2024-03-28 06:08:02 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,10 @@
+-- SQL script to create MySQL user user_0d_1 with all privileges
+
+-- Check if user_0d_1 already exists
+SELECT user FROM mysql.user WHERE user = 'user_0d_1';
+
+-- If user_0d_1 does not exist, create the user and grant all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
