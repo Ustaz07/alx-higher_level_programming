a2c3579e1f3c36e9ebc695a7aae45612a91ca15b
Ustaz07 <shamsutukor@gmail.com>
2024-04-24 03:47:49 -0700

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,6 @@
-exports.callMeMoby = function (x, theFunction) {
-  // Validate the parameters
-  if (typeof x !== 'number' || !Number.isInteger(x) || x <= 0) {
-    throw new Error('The first parameter must be a positive integer.');
-  }
-  if (typeof theFunction !== 'function') {
-    throw new Error('The second parameter must be a function.');
-  }
+#!/usr/bin/node
 
-  // Call the function x times
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
