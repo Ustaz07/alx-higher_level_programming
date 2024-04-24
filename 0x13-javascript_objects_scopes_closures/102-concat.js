2c7b1c355cb47faf80c9069fc10cc309653a1e54
Ustaz07 <shamsutukor@gmail.com>
2024-04-24 06:56:48 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,33 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, theFunction) {
-  number += 1;
-  theFunction(number);
-};
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js fileA fileB fileC');
+  process.exit(1);
+}
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const concatenatedData = dataA + dataB;
+    fs.writeFile(fileC, concatenatedData, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`Files ${fileA} and ${fileB} successfully concatenated into ${fileC}`);
+    });
+  });
+});
