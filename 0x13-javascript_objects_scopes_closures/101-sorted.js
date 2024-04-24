12d76265d80ccd02c9239f1992ae856c93bc67cd
Ustaz07 <shamsutukor@gmail.com>
2024-04-24 03:51:43 -0700

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,24 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+// Import the dict object from the 101-data.js file
+const { dict } = require('./101-data');
+
+// Function to swap keys and values in a dictionary
+function swapKeysAndValues(obj) {
+  const result = {};
+  for (const key in obj) {
+    const value = obj[key];
+    if (!result[value]) {
+      result[value] = [key];
+    } else {
+      result[value].push(key);
+    }
   }
-};
+  return result;
+}
+
+// Create a new dictionary with occurrences as keys and lists of user ids as values
+const sortedDict = swapKeysAndValues(dict);
+
+// Print the new dictionary
+console.log(sortedDict);
