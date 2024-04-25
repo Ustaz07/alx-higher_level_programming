5b4eedd80e9402395242ffb518a229199ed34d01
Ustaz07 <shamsutukor@gmail.com>
2024-04-25 05:41:18 -0700

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,24 +1,15 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12
 };
-
 console.log(myObject);
-
-myObject.value += 1;
-myObject.incr = function () {
-  myObject.value += 1;
-};
-
-console.log(myObject);
-
+/*
+YOUR CODE HERE
+*/
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
