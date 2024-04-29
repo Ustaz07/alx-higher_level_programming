26de7ba7695a6b21e16040e10053304b186e8fef
Ustaz07 <shamsutukor@gmail.com>
2024-04-29 06:45:05 -0700

    6-model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-"""
-Script that lists all cities of a specific state from the database hbtn_0e_4_usa
-"""
+"""contains the class definition of a State and an instance
+Base = declarative_base() """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == '__main__':
-    # Connect to MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    # Create all tables in the engine
-    Base.metadata.create_all(engine)
 
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+Base = declarative_base()
 
-    # Check if the state exists
-    state = session.query(State).filter_by(name=sys.argv[4]).first()
-    if state:
-        cities = session.query(State, City).join(City).filter(State.name == sys.argv[4]).all()
-        for city in cities:
-            print(city[1].name)
-    else:
-        print("")
 
-    # Close the session
-    session.close()
+class State(Base):
+    """ State Class inherit from Base declarative_base()
+    links to the MySQL table states
+    Attr:
+        id, name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
