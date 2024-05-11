ba11aba954266b507ab9ff0960146c6e25777e3c
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 02:43:26 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that sends a POST request to a specified URL with an email as a parameter
+Script that sends a POST request to a
+specified URL with an email as a parameter
 and displays the body of the response.
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
@@ -8,6 +9,7 @@ Usage: ./6-post_email.py <URL> <email>
 
 import sys
 import requests
+
 
 def main():
     if len(sys.argv) != 3:
@@ -21,10 +23,11 @@ def main():
 
     try:
         response = requests.post(url, data=payload)
-        response.raise_for_status()  # Raise an error for bad response status
+        response.raise_for_status()  # Bad response status
         print(response.text)
     except requests.RequestException as e:
         print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     main()
