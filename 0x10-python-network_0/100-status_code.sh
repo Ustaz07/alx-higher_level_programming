940eee2a9a59802ee71487368682cb5012eae2b0
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 07:27:34 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -2,5 +2,11 @@
 # Sends a HEAD request to the specified URL and displays only the HTTP status code.
 # Usage: ./get_status_code.sh <URL>
 
-if [ $# -ne 1 ]; then echo "Usage: $0 <URL>"; exit 1; fi
+# Check if a URL is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a HEAD request to the specified URL and output only the HTTP status code
 curl -s -o /dev/null -w "%{http_code}" "$1"
