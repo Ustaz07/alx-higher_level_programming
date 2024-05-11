ddb821793d3ca9b4c42cad2d4af64d770015c7af
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 07:26:39 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,31 +1,6 @@
-#!/usr/bin/python3
-"""
-Script to list the 10 most recent commits on a given GitHub repository.
-"""
-import sys
-import requests
+#!/bin/bash
+# Sends a HEAD request to the specified URL and displays only the HTTP status code.
+# Usage: ./get_status_code.sh <URL>
 
-def list_recent_commits(repo_owner, repo_name):
-    """
-    Lists the 10 most recent commits on a GitHub repository.
-    """
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()[:10]
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print("Error:", response.status_code)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./script.py <repo_owner> <repo_name>")
-        sys.exit(1)
-
-    repo_owner = sys.argv[1]
-    repo_name = sys.argv[2]
-    list_recent_commits(repo_owner, repo_name)
+if [ $# -ne 1 ]; then echo "Usage: $0 <URL>"; exit 1; fi
+curl -s -o /dev/null -w "%{http_code}" "$1"
