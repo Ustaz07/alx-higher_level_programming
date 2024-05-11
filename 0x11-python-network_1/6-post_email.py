9213f459d2cc81b62ea1b8614671653ba6d88435
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 02:31:47 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,23 +4,15 @@ Script that takes in a URL and an email address,
 sends a POST request to the URL with the email as a parameter,
 and finally displays the body of the response using the requests package.
 """
-
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python3 script.py <URL> <email>")
-        sys.exit(1)
-
     url = sys.argv[1]
     email = sys.argv[2]
-
     payload = {'email': email}
+    response = requests.post(url, data=payload)
     
-    try:
-        response = requests.post(url, data=payload)
-        response.raise_for_status()  # Raise an error for bad response status
-        print("Your email is:", response.text.strip())  # Strip whitespace from the response
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+    # Extract email from response body
+    email_response = response.text.split("Email: ")[1].strip()
+    print("Your email is:", email_response)
