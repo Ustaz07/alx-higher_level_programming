4d9e41c9f1fd25571f6c8e240969445e1328d23d
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 06:43:03 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl - sL "$1"
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL -w "%{http_code}" -o /dev/null "$1" | grep -q 200 && curl -sL "$1"
