ad42ee30a9202fbc00ef3b7a9452ee5e3f4a2a53
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 07:12:47 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Sends a GET request to a given URL and display the response status code.
+# This script sends a request to a URL passed as an argument and displays only the status code of the response
+
+# Send a HEAD request to the specified URL and output only the HTTP status code
 curl -s -o /dev/null -w "%{http_code}" "$1"
