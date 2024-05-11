6a60d89c755afa492765fa950710d3c0fd03c6cb
Ustaz07 <shamsutukor@gmail.com>
2024-05-11 07:06:32 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
-curl - s - X POST - d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# This script takes in a URL, sends a POST request to the passed URL with specific parameters, and displays the body of the response
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
