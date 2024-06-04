de9f6d994ffe039f5fb07a20a819ced0b8f20a59
Ustaz07 <shamsutukor@gmail.com>
2024-06-04 06:23:59 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+if (!filePath || !content) {
+  console.error('Error: Missing arguments. Usage: ./1-writeme.js <file path> <string to write>');
+  process.exit(1);
+}
+
+fs.writeFile(filePath, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`File written successfully: ${filePath}`);
+});
