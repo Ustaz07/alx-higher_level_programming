5a47d8bf91f8541e4dd1d1731beb3b2ce76a5d11
Ustaz07 <shamsutukor@gmail.com>
2024-06-04 06:43:26 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,29 +1,5 @@
-const request = require('request');
+#!/usr/bin/node
 const fs = require('fs');
-
-// Function to fetch data from a URL and write it to a file
-function fetchDataAndWriteToFile(url, filePath) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        fs.writeFile(filePath, body, 'utf8', err => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        });
-      }
-    });
-  });
-}
-
-// Usage example
-const url = 'https://example.com';
-const filePath = 'example.html';
-
-fetchDataAndWriteToFile(url, filePath)
-  .then(() => console.log('File written successfully'))
-  .catch(error => console.error('Error:', error));
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
