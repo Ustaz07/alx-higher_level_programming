288a209126e0204b700e4b12df0c7549fc4e4005
Ustaz07 <shamsutukor@gmail.com>
2024-06-11 23:43:09 -0700

    1-script.js

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Wait for the document to be fully loaded before executing the script
+$(document).ready(function() {
+    // Select the <header> element using JQuery and change its text color to red
+    $('header').css('color', '#FF0000');
+});
