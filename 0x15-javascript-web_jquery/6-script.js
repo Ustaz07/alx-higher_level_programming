3525014f2aeb592e5b4260375e348fefa6b334a5
Ustaz07 <shamsutukor@gmail.com>
2024-06-12 07:18:12 -0700

    5-script.js

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#update_header').click(function(){
+        // Select the <header> element and update its text content
+        $('header').text('New Header!!!');
+    });
+});
