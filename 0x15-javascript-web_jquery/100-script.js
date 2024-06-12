c49d915298df4d94545344fd1abfc5ab752ae247
Ustaz07 <shamsutukor@gmail.com>
2024-06-12 07:40:38 -0700

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the <header> element
+    var header = document.querySelector('header');
+    
+    // Update its text color to red
+    header.style.color = '#FF0000';
+});
